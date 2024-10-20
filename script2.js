0724b63bd470e7e8de81b01176669234e8be29f8
Davide Bianchin <bianchindavide@itis-molinari.eu>
2024-10-20 13:28:47 +0200

    creazione funzione per creare la tabella createTable

--- script2.js
+++ script2.js
@@ -2,17 +2,57 @@
 
 
 /* Funzione per le Date */
-const getDateKey = (date) => 
-    date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
-  
-  let date = new Date();
-  const datekeys = [];
-  
-  for (let i = 0; i < 30; i++) {
-    date.setDate(date.getDate() + 1);
-    const key = getDateKey(date);
-    datekeys.push(key);
-  }
-  
-  console.log(datekeys);
-  
+const fDates = () => {
+    const dates = [];
+    const today = new Date();
+
+    for (let i = 0; i < 30; i++){
+        const newDate = new Date();
+        newDate.setDate(today.getDate() + i);
+
+        const day = ('0' + newDate.getDate()).slice(-2);
+        const month = ('0' + newDate.getDate()).slice(-2);
+        const year = newDate.getFullYear(); 
+    
+        const formattedDate = `${year}-${month}-${day}`;
+        dates.push(formattedDate);
+    }
+    return dates;
+}
+
+/* Funzione per la creazione della Tabella*/
+const createTable = (information) => {
+    const div = document.getElementById("TableID");
+
+    let tableHTML = `
+    <table>
+        <thead>    
+            <tr> 
+                <th>Data</th>
+                <th>Singola</th>
+                <th>Doppia</th>
+                <th>Suite</th>
+            </tr>
+        </thead>
+    </table>
+    <tbody>
+    `;
+
+    fDates().forEach(date => {
+        tableHTML += `
+            <tr>
+                <td>${date}</td>
+                <td>${information[date]["Singola"]}</td>
+                <td>${information[date]["Doppia"]}</td>
+                <td>${information[date]["Suite"]}</td>
+            </tr>
+        `;
+    });
+
+    tableHTML += `
+        </tbody>
+    </table>
+    `;
+
+    div.innerHTML += tableHTML;
+}
