b224c09e90a51e6ede6a053a1145c5897c404549
Davide Bianchin <bianchindavide@itis-molinari.eu>
2024-10-21 21:10:47 +0200

    tabella 1.0

--- script2.js
+++ script2.js
@@ -1,58 +1,103 @@
-/* TABELLA */
+/* Table */
+
+let availabilityData = {};
+const conf = {
+  singola: 10,
+  doppia: 5,
+  suite: 3
+};
 
 
-/* Funzione per le Date */
-const fDates = () => {
-    const dates = [];
-    const today = new Date();
-
-    for (let i = 0; i < 30; i++){
-        const newDate = new Date();
-        newDate.setDate(today.getDate() + i);
-
-        const day = ('0' + newDate.getDate()).slice(-2);
-        const month = ('0' + newDate.getDate()).slice(-2);
-        const year = newDate.getFullYear(); 
-    
-        const formattedDate = `${year}-${month}-${day}`;
-        dates.push(formattedDate);
-    }
-    return dates;
+function initializeAvailability() {
+  const dates = fDates();
+  for (let i = 0; i < dates.length; i++) {
+    availabilityData[dates[i]] = {
+      Singola: conf.singola,
+      Doppia: conf.doppia,
+      Suite: conf.suite
+    };
+  }
 }
 
-/* Funzione per la creazione della Tabella*/
-const createTable = (information) => {
-    const div = document.getElementById("TableID");
+function fDates() {
+  const dates = [];
+  const today = new Date();
 
-    let tableHTML = `
-    <table>
-        <thead>    
-            <tr> 
-                <th>Data</th>
-                <th>Singola</th>
-                <th>Doppia</th>
-                <th>Suite</th>
-            </tr>
-        </thead>
-    </table>
-    <tbody>
-    `;
+  for (let i = 0; i < 30; i++) {
+    const newDate = new Date();
+    newDate.setDate(today.getDate() + i);
 
-    fDates().forEach(date => {
-        tableHTML += `
-            <tr>
-                <td>${date}</td>
-                <td>${information[date]["Singola"]}</td>
-                <td>${information[date]["Doppia"]}</td>
-                <td>${information[date]["Suite"]}</td>
-            </tr>
-        `;
-    });
+    const day = ('0' + newDate.getDate()).slice(-2);
+    const month = ('0' + (newDate.getMonth() + 1)).slice(-2);
+    const year = newDate.getFullYear();
 
+    const formattedDate = year + '-' + month + '-' + day;
+    dates.push(formattedDate);
+  }
+
+  return dates;
+}
+
+function createTable(information) {
+  const div = document.getElementById("TableID");
+  
+  let tableHTML = `
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Singola</th>
+          <th>Doppia</th>
+          <th>Suite</th>
+        </tr>
+      </thead>
+      <tbody>
+  `;
+
+  const dates = fDates();
+  for (let i = 0; i < dates.length; i++) {
     tableHTML += `
-        </tbody>
-    </table>
+      <tr>
+        <td>${dates[i]}</td>
+        <td>${information[dates[i]].Singola}</td>
+        <td>${information[dates[i]].Doppia}</td>
+        <td>${information[dates[i]].Suite}</td>
+      </tr>
     `;
+  }
 
-    div.innerHTML += tableHTML;
+  tableHTML += `
+      </tbody>
+    </table>
+  `;
+
+  div.innerHTML = tableHTML;
 }
+
+function addReservation(reservation) {
+  const date = reservation.date;
+  const roomsBooked = reservation.roomsBooked;
+
+
+  for (let i = 0; i < roomsBooked.length; i++) {
+    const roomType = roomsBooked[i].room;
+    const quantity = roomsBooked[i].quantity;
+    availabilityData[date][roomType] -= quantity;
+  }
+
+  createTable(availabilityData);
+}
+
+
+initializeAvailability();
+createTable(availabilityData);
+
+const exampleReservation = {
+  date: "2025-03-15",
+  roomsBooked: [
+    { room: 'Singola', quantity: 2 },
+    { room: 'Suite', quantity: 1 }
+  ]
+};
+
+addReservation(exampleReservation);
