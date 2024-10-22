afc22593ca3b1c75ab0b122414a7a1ff95f16a22
Gianluca Brambilla <brambillagianluca@itis-molinari.eu>
2024-10-22 13:28:07 +0200

    form

--- script1.js
+++ script1.js
@@ -1,11 +1,10 @@
-//form
-
 const getDateKey = (date) => date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
 
 let date = new Date();
 const datekeys = [];
 for (let i = 0; i < 30; i++) {
     date.setDate(date.getDate() + 1);
+
     const key = getDateKey(date);
     datekeys.push(key);
 }
@@ -28,26 +27,30 @@ const createForm = (parentElement) => {
             callback = callbackInput;
         },
         render: () => {
-            parentElement.innerHTML = `
+            let formHtml = `
         <div>
           Data prenotazione:<br>
           <input id="bookingDate" type="date" />
-        </div>
-        ${roomTypes.map((room) => {
+        </div>`;
+            
+            formHtml += roomTypes.map((room) => {
                 return `
-          <div>${room}  Numero di camere:<br>
-              <input id="${room} rooms" type="number" />
+          <div>` + room + `  Numero di camere:<br>
+              <input id="` + room + `rooms" type="number" />
             </div>`;
-            }).join('\n')}
-        <button type='button' id='submit'>Invio</button>
+            }).join('\n');
+
+            formHtml += `<button type='button' id='submit'>Invio</button>
         <div id="ris"></div>`;
+
+            parentElement.innerHTML = formHtml;
 
             document.querySelector("#submit").onclick = () => {
                 const bookingDate = document.querySelector("#bookingDate").value;
                 const roomsBooked = roomTypes.map((room) => {
                     return {
                         room,
-                        quantity: parseInt(document.querySelector(`#${room}rooms`).value),
+                        quantity: parseInt(document.querySelector("#" + room + "rooms").value),
                         maxQuantity: conf[room]
                     };
                 });
@@ -62,14 +65,14 @@ const createForm = (parentElement) => {
                 let availableDate = bookingDate !== '';
                 const result = document.querySelector("#ris");
                 if (availableDate && OneRoomAvailable) {
-                    result, innerText = "OK";
+                    result.innerText = "OK";
                     callback({
                         availableDate,
                         roomsBooked: roomsBooked.filter(c => c.quantity > 0)
                     });
                     document.querySelector("#bookingDate").value = '';
                     roomTypes.forEach(room => {
-                        document.querySelector(`#${room}rooms`).value = '';
+                        document.querySelector("#" + room + "rooms").value = '';
                     });
                 } else {
                     result.innerText = "KO";
@@ -83,4 +86,3 @@ const form = createForm(document.querySelector('#FormDIV'));
 form.setRoomTypes(Object.keys(conf));
 form.onsubmit(console.log);
 form.render();
-
